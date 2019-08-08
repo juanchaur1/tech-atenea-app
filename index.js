a0c41504def4f0d2786ac3234c82bcd5c416b3ce
juanchaur1 <juan.chaur@marfeel.com>
2019-08-08 14:07:37 -0400

    changes

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('pull_request.closed', async context => {
+    app.log(context)
+
+    const headRepoId = context.payload.pull_request.head.repo.id
+    const baseRepoId = context.payload.pull_request.base.repo.id
+
+    app.log(`HeadRepoId: ${headRepoId}`)
+    app.log(`baseRepoId: ${baseRepoId}`)
+
+
+    if (!context.payload.pull_request.merged) {
+      context.log.info(`PR was closed but not merged. Keeping`)
+    }
+    
+    // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    // return context.github.issues.createComment(issueComment)
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
